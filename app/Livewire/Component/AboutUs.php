27220d6fa4f209ddf1f53ea40c4ed81f72dc8871
ZhafranBahij <muhammadzhafranbahij@gmail.com>
2025-07-09 10:08:56 +0700

    feat: service

--- app/Livewire/Component/AboutUs.php
+++ app/Livewire/Component/AboutUs.php
@@ -3,18 +3,22 @@
 namespace App\Livewire\Component;
 
 use App\Models\AboutUs as ModelsAboutUs;
+use App\Models\Service;
 use Livewire\Component;
 
 class AboutUs extends Component
 {
 
-    public $about_us;
+    public $about_us, $services;
 
     public function mount()
     {
         $this->about_us = ModelsAboutUs::query()
                             ->latest()
                             ->first();
+
+        $this->services = Service::query()
+                            ->get();
     }
 
     public function render()
--- resources/views/livewire/component/about-us.blade.php
+++ resources/views/livewire/component/about-us.blade.php
@@ -15,22 +15,12 @@
         <div class="col-lg-6">
           <div class="info-table">
             <ul>
-              <li>
-                <img src="assets/images/info-icon-01.png" alt="" style="max-width: 52px;">
-                <h4>250 m2<br><span>Total Flat Space</span></h4>
-              </li>
-              <li>
-                <img src="assets/images/info-icon-02.png" alt="" style="max-width: 52px;">
-                <h4>Contract<br><span>Contract Ready</span></h4>
-              </li>
-              <li>
-                <img src="assets/images/info-icon-03.png" alt="" style="max-width: 52px;">
-                <h4>Payment<br><span>Payment Process</span></h4>
-              </li>
-              <li>
-                <img src="assets/images/info-icon-04.png" alt="" style="max-width: 52px;">
-                <h4>Safety<br><span>24/7 Under Control</span></h4>
-              </li>
+              @foreach ($services as $service)
+                <li>
+                  <img src="{{ 'storage/'.$service->icon }}" alt="" style="max-width: 52px;">
+                  <h4>{{ $service->title }}<br><span>{{ $service->description }}</span></h4>
+                </li>
+              @endforeach
             </ul>
           </div>
         </div>
